0f30d7d2c8eea2e4df6e7e37d6e7fe94c45a2fce
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2022-06-16 03:23:59 +0530

    package and db added

---
+++ db.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const mongoURI = "mongodb+srv://taparia:220807@cluster0.fbjet.mongodb.net/wastecollector"
+
+const connectToMongo = ()=>{
+    mongoose.connect(mongoURI, ()=>{
+        console.log("connected to Mongo Successfully")
+    })
+}
+
+module.exports = connectToMongo;
---
+++ index.js
@@ -0,0 +1,21 @@
+const connectToMongo = require('./db');
+const express = require('express')
+var cors = require('cors')
+
+connectToMongo();
+const app = express()
+const port = 5000
+
+app.use(cors())
+app.use(express.json())
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// Available routes 
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
